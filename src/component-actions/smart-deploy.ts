5cfff6847ac959409d28e8f516dc1cd71d192530
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-04-28 13:22:14 -0700

    refactor(smart-deploy.ts): [WIP] more updates to smart-deploy

--- src/component-actions/smart-deploy.ts
+++ src/component-actions/smart-deploy.ts
@@ -3,6 +3,8 @@ import { ComponentActionError } from '../error';
 import { ComponentAction } from '../../types/global';
 import { installPrivilegedDependencies } from '../utils/install';
 import { downloadPaths } from '../utils/github';
+import { existsSync } from 'fs';
+import { toss } from 'toss-expression';
 import metadataDownload from './metadata-download';
 import debugFactory from 'debug';
 import execa from 'execa';
@@ -12,51 +14,63 @@ import type { RunnerContext, InvokerOptions } from '../../types/global';
 const debug = debugFactory(`${pkgName}:${ComponentAction.SmartDeploy}`);
 
 export default async function (context: RunnerContext, options: InvokerOptions) {
-  if (!options.npmToken)
-    throw new ComponentActionError('missing required option `npmToken`');
+  !options.npmToken &&
+    toss(new ComponentActionError('missing required option `npmToken`'));
 
-  if (!options.githubToken)
-    throw new ComponentActionError('missing required option `githubToken`');
+  !options.githubToken &&
+    toss(new ComponentActionError('missing required option `githubToken`'));
 
-  if (!options.gpgPrivateKey)
-    throw new ComponentActionError('missing required option `gpgPrivateKey`');
+  !options.gpgPrivateKey &&
+    toss(new ComponentActionError('missing required option `gpgPrivateKey`'));
 
-  if (!options.gpgPassphrase)
-    throw new ComponentActionError('missing required option `gpgPassphrase`');
+  !options.gpgPassphrase &&
+    toss(new ComponentActionError('missing required option `gpgPassphrase`'));
 
-  if (!options.codecovToken) debug('no codecov token provided (OK)');
+  !options.codecovToken && debug('no codecov token provided (OK)');
 
-  // TODO: still do semantic-release but do not npm publish if private == true
-  // TODO: do not checkout if only automerge
-  // TODO: replace all metadata calls with destructuring style project-wide
-  const { shouldSkipCi, shouldSkipCd, commitSha } = await metadataDownload(context, {
+  const {
+    shouldSkipCi,
+    shouldSkipCd,
+    commitSha,
+    hasPrivate,
+    canRelease,
+    canAutomerge
+  } = await metadataDownload(context, {
     enableFastSkips: true,
+    repository: {
+      checkoutRef: false
+    },
     ...options
   });
 
   if (!shouldSkipCi && !shouldSkipCd) {
-    // * Prepare environment
-    // TODO: setup env for GPG and semantic-release
-    // TODO: checkout repo but with empty working tree to ./artifact
-    // TODO: setup node 15
-
-    await installPrivilegedDependencies();
-
     // * Download, unpack, and verify build artifact
-    await downloadPaths(`build-${process.env.RUNNER_OS}-${commitSha}`, './artifact');
-    // TODO: check ./artifact does not have a node_modules or .git directory and error if it does
+    await downloadPaths(`build-${process.env.RUNNER_OS}-${commitSha}`);
 
-    // * Merge privileged dependencies with build artifact
-    await execa('mv', ['node_modules', './artifact'], { stdio: 'inherit' });
-    await execa('cd', ['./artifact'], { stdio: 'inherit' });
+    if (existsSync('./node_modules')) {
+      throw new ComponentActionError(
+        'illegal build artifact: encountered unexpected node_modules'
+      );
+    }
 
-    // * Setup GPG auth
-    // TODO: Setup GPG (see gpg action source)
+    if (canRelease) {
+      await execa('mv', ['package.json', 'package.json-actual'], { stdio: 'inherit' });
+      await execa('mv', ['package.json-actual', 'package.json'], { stdio: 'inherit' });
 
-    // * Attempt to release
-    await execa('npx', ['--no-install', 'semantic-release'], { stdio: 'inherit' });
+      // * Install npm dependencies
+      await installPrivilegedDependencies();
+
+      // * Setup GPG auth
+      // TODO: Setup GPG (see gpg action source)
+
+      // * Attempt to release
+      await execa('npx', ['--no-install', 'semantic-release'], {
+        stdio: 'inherit',
+        env: { NPM_IS_PRIVATE: hasPrivate ? 'true' : undefined }
+      });
+    } else if (canAutomerge) {
+      // TODO: On auto-merge, don't retry on anything less than 500 except 408,
+      // TODO: 429, and the expected retry codes
+    }
   } else debug(`skipped component action "${ComponentAction.SmartDeploy}"`);
-
-  // TODO: On auto-merge, don't retry on anything less than 500 except 408, 429,
-  // TODO: and the expected retry codes
 }
